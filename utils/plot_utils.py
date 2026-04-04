a6b008ba51d7247b13c8a4b69e4a1fef86eda6db
Simona Miller <smiller@caltech.edu>
2026-04-03 20:40:37 -0700

    push figure 5

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -6,6 +6,12 @@ run_labels = {
     'gaussian_likelihood_sigma_0.5':r'Gaussian $\mathcal{L}$, $\sigma_{\rm meas} = 0.5$',
     'bilby_likelihood':'Realistic O3 noise $\mathcal{L}$'
 }
+run_labels_full = {
+    'gaussian_likelihood_sigma_0.1':r'Gaussian likelihood, $\sigma_{\rm meas} = 0.1$',
+    'gaussian_likelihood_sigma_0.3':r'Gaussian likelihood, $\sigma_{\rm meas} = 0.3$',
+    'gaussian_likelihood_sigma_0.5':r'Gaussian likelihood, $\sigma_{\rm meas} = 0.5$',
+    'bilby_likelihood':'Realistic O3 noise likelihood'
+}
 run_colors = {
     'gaussian_likelihood_sigma_0.1':'#FF5733', 
     'gaussian_likelihood_sigma_0.3':'#6A6BAD',
@@ -103,3 +109,58 @@ def fraction_underpredicted(pred_obs, params, ncut=4, nbins=50):
         }  
 
     return slopeDict, percDict
+
+
+# Functions that tell us the various T (test statistics)
+def mean(x): 
+    return np.average(x)
+def width(x): 
+    return np.std(x)
+def zero_positive_spin(cat):
+    positive_frac = np.sum(cat > .33)
+    zero_frac = np.sum( (cat <= .33) & (cat >= -.33) )
+    return zero_frac / positive_frac
+def fraction_tails(x): 
+    return sum(np.abs(x)>0.5)/len(x)
+
+def get_test_statistics(pred_obs):
+    traces_dict = {
+    k:{
+        i:{
+            'y_rep':np.concatenate([v['predicted']['costheta1'][i], v['predicted']['costheta2'][i]]), 
+            'y':np.concatenate([v['observed']['costheta1'][i], v['observed']['costheta2'][i]])
+        } for i in range(len(v['predicted']['costheta1']))
+    }
+        for k,v in pred_obs.items()  
+    }
+    T_dict = {k:get_Tdict(v) for k,v in traces_dict.items()}
+    return T_dict
+
+zp_title = r'$\frac{N(\cos\theta\in[-0.33, 0.33])}{N(\cos\theta\in[0.33, 1])}$'    
+
+def get_Tdict(PPC_traces_event, verbose=False):
+    T_dict_event = {} # store test statistics
+    for func_name, func in zip(
+        ['mean', 'standard deviation',zp_title, r'fraction $|\cos\theta\,|>0.5$'],
+        [mean, width, zero_positive_spin, fraction_tails]
+    ): 
+        # Calculate different test statistics from 'y'  and 'y_rep' in each entry of PPC_traces_event
+        Ntraces = len(PPC_traces_event)
+        T_obs_arr = np.zeros(Ntraces)
+        T_rep_arr = np.zeros(Ntraces)
+        for i,p in enumerate(PPC_traces_event.values()):
+            T_obs_arr[i] = func(p['y']) 
+            T_rep_arr[i] = func(p['y_rep'])
+        
+        # Calculate p value by comparing them
+        percentile = sum(T_obs_arr < T_rep_arr) / Ntraces
+        pvalue = 1 - 2 * np.abs(percentile-0.5)
+    
+        # Print info
+        if verbose:
+            print(func_name, percentile, pvalue)
+    
+        # Save to ongoing dict
+        T_dict_event[func_name] = dict(pvalue=np.round(pvalue,3), T=T_obs_arr, Trep=T_rep_arr)
+    
+    return T_dict_event
