6b17f05c57778212fd44f0ee5e1d8c9881610643
Simona Miller <simona.miller@ldas-pcdev5.ligo-la.caltech.edu>
2026-04-06 19:20:10 -0500

    push figs 10 and 14

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.colors as mcolors
 
 run_labels = {
     'gaussian_likelihood_sigma_0.1':r'Gaussian $\mathcal{L}$, $\sigma_{\rm meas} = 0.1$',
     'gaussian_likelihood_sigma_0.3':r'Gaussian $\mathcal{L}$, $\sigma_{\rm meas} = 0.3$',
     'gaussian_likelihood_sigma_0.5':r'Gaussian $\mathcal{L}$, $\sigma_{\rm meas} = 0.5$',
-    'bilby_likelihood':'Realistic O3 noise $\mathcal{L}$'
+    'bilby_likelihood':r'Realistic O3 noise $\mathcal{L}$'
 }
 run_labels_full = {
     'gaussian_likelihood_sigma_0.1':r'Gaussian likelihood, $\sigma_{\rm meas} = 0.1$',
@@ -32,11 +33,42 @@ run_colors_darker = {
 }
 
 
+GWTC4_chi_color = '#ff9408'
+GWTC4_tilt_color = '#6488ea'
+
+GWTC4_labels_data_vs_event = {
+    'a':{
+        'event':{
+            'label':r'$\chi_{\rm true}$',
+            'label pred':r'$\chi_{\rm true}^{\rm pred}$',
+            'label obs':r'$\chi_{\rm true}^{\rm obs}$',
+        }, 
+        'data':{
+            'label':r'$\chi_{\mathrm{max}.\mathcal{L}}$',
+            'label pred':r'$\chi_{\mathrm{max}.\mathcal{L}}^{\rm pred}$',
+            'label obs':r'$\chi_{\mathrm{max}.\mathcal{L}}^{\rm obs}$',
+        }
+    },
+    'tilt':{
+        'event':{
+            'label':r'$\cos\theta_{\rm true}$',
+            'label pred':r'$\cos\theta_{\rm true}^{\rm pred}$',
+            'label obs':r'$\cos\theta_{\rm true}^{\rm obs}$',
+        }, 
+        'data':{
+            'label':r'$\cos\theta_{\mathrm{max}.\mathcal{L}}$',
+            'label pred':r'$\cos\theta_{\mathrm{max}.\mathcal{L}}^{\rm pred}$',
+            'label obs':r'$\cos\theta_{\mathrm{max}.\mathcal{L}}^{\rm obs}$',
+        }
+    }
+}
+
+
 def average_curve(pred_obs, param):    
 
     allpred = np.sort(pred_obs['predicted'][param])
     allobs = np.sort(pred_obs['observed'][param])
-
+    
     pred_avgs = np.average(allpred, axis=0)
     obs_avgs = np.average(allobs, axis=0)
 
@@ -163,3 +195,101 @@ def get_Tdict(PPC_traces_event, verbose=False):
         T_dict_event[func_name] = dict(pvalue=np.round(pvalue,3), T=T_obs_arr, Trep=T_rep_arr)
     
     return T_dict_event
+
+
+def darken_color(color, factor=0.7):
+    """
+    Darken a color by multiplying its lightness.
+
+    Parameters
+    ----------
+    color : str or tuple
+        Any matplotlib-compatible color.
+    factor : float, optional
+        Amount to darken (0–1).
+        1.0 = no change
+        0.0 = black
+        Default is 0.7.
+
+    Returns
+    -------
+    tuple
+        Darkened RGB color tuple.
+    """
+    # Convert color to RGB
+    rgb = mcolors.to_rgb(color)
+
+    # Multiply each channel
+    darker_rgb = tuple(max(0, c * factor) for c in rgb)
+
+    return darker_rgb
+
+
+def traces_and_underpred_plot(axes, DD, PD, param, color='C0', ntraces=1000, lbl=None, plot_avg=True): 
+    
+    bounds = {
+        'a':[0,1],
+        'cos_tilt':[-1,1],
+    }
+    labels = {
+        'a':r'$\chi$',
+        'cos_tilt':r'$\cos\theta$',
+    }
+    color_dark = darken_color(color, factor=0.6)
+    
+    if lbl is None:
+        label = labels[param]
+        label_pred = 'predicted '+label
+        label_obs = 'observed '+label
+    else: 
+        label = lbl['label']
+        label_pred = lbl['label pred']
+        label_obs = lbl['label obs']
+        
+    diag_kws = dict(color='k', ls='--', lw=0.5)
+    
+    # ======================
+    # ===== PPC TRACES =====
+    # ======================
+    ax = axes[0]
+    for obs_trace, pred_trace in zip(
+        DD['observed'][param][:ntraces], 
+        DD['predicted'][param][:ntraces]
+    ):
+        ax.plot(pred_trace, obs_trace, lw=0.3, alpha=0.03, color=color)
+    
+    b = bounds[param]
+    ax.plot(b,b,**diag_kws)
+    ax.set_xlim(*b)
+    ax.set_ylim(*b)
+    ax.grid()
+
+    ax.set_xlabel(label_pred, fontsize=13)
+    ax.set_ylabel(label_obs, fontsize=13)
+    
+    if plot_avg: 
+        obs_avgs, pred_avgs = average_curve(DD, param)
+        ax.plot(pred_avgs, obs_avgs, color=color_dark, lw=0.7)
+    
+    # ======================
+    # = FRACTION UNDERPRED =
+    # ======================
+    ax = axes[1]
+    
+    x = PD[param]['xs']
+    y = PD[param]['fraction']
+    N = PD[param]['N']
+
+    err = 1 / np.sqrt(N)
+
+    ax.scatter(x, y, color=color_dark, s=2, marker='*')
+    ax.fill_between(x, y+err, y-err, alpha=.3, color=color)
+
+    b = bounds[param]
+    ax.set_xlim(*b)
+    ax.set_ylim(0,1)
+    ax.axhline(0.5, **diag_kws)
+    ax.grid()
+
+    ax.set_xlabel(label, fontsize=13)
+    ax.set_ylabel('Fraction underpredicted', fontsize=11)
